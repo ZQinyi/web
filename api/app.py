6ea95b7a9f1437e9fe99b8d1c8a5d71afdc21ed6
qz23 <qz23@imperial.ac.uk>
2023-10-23 15:24:16 +0100

    flake8

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/") 
+
+@app.route("/")
 def hello_world():
     return render_template("index.html")
 
@@ -10,8 +11,10 @@ def hello_world():
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-    input_major = request.form.get("major");
-    return render_template("hello.html", name=input_name, age=input_age, major=input_major)
+    input_major = request.form.get("major")
+    return render_template("hello.html", name=input_name, age=input_age,
+                           major=input_major)
+
 
 @app.route("/info", methods=["GET"])
 def info():
